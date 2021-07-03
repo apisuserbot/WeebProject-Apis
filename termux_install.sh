1029f3a0693d7dac05a72f05ed9fd16fd4d38e7e
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-07-03 07:49:01 +0700

    fixes

--- termux_install.sh
+++ termux_install.sh
@@ -35,7 +35,7 @@ pkg update && pkg upgrade -y
 pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
 
 git clone https://github.com/apisuserbot/WeebProject-Apis.git
-cd Lord-Userbot
+cd WeebProject-Apis
 
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
