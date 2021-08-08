0a13c59e44abc24282b00a92be81905418c91e74
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-08 07:30:19 +0700

    ...

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -17,8 +17,8 @@ from pylast import LastFMNetwork, md5
 from pySmartDL import SmartDL
 from dotenv import load_dotenv
 from requests import get
-from telethon.sync import TelegramClient, custom, events
-from telethon import version
+from telethon.sync import custom, events
+from telethon import TelegramClient, version
 from telethon.sessions import StringSession
 from telethon import Button, functions, types
 from telethon.utils import get_display_name
