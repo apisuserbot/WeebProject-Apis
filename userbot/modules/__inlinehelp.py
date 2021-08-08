3615a7a7ae4bbb5c537fc7e53ee668b747999906
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-08 08:51:16 +0700

    ...

--- userbot/modules/__inlinehelp.py
+++ userbot/modules/__inlinehelp.py
@@ -17,7 +17,7 @@ async def yardim(event):
     try:
         tgbotusername = BOT_USERNAME
         if tgbotusername is not None:
-            results = await event.client.inline_query(tgbotusername, "@Python")
+            results = await event.client.inline_query(tgbotusername, "@KingUserbotSupport")
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
